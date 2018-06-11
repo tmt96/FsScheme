8ed09d28f1f99873f987d00d2705237c3f81e8be
tmt <mt12@williams.edu>
2018-06-11 17:03:44 +0700

    added LispVal

---
+++ README.md
@@ -0,0 +1,11 @@
+# FsScheme
+
+A basic Scheme-subset interpreter written in F#.
+
+## Motivation
+
+I have been intended to learn about functional programming and compiler writing for a while. I have done the [Write Yourself a Scheme in 48 hours by Jonathan Tang](https://upload.wikimedia.org/wikipedia/commons/a/aa/Write_Yourself_a_Scheme_in_48_Hours.pdf) but never has the feel for the language. This is a second attempt, in F#.
+
+## Credit
+
+The project is done by porting [Write Yourself a Scheme, version 2.0 by Adam Wespiser](https://www.wespiser.com/writings/wyas/00_overview.html) from Haskell to F#, with reference to [the "original" port by Luca Bolognese](https://lucabolognese.wordpress.com/2011/06/30/write-yourself-a-scheme-in-48-hours-in-f-part-i/)
---
+++ src/Library/LispVal.fs
@@ -0,0 +1,42 @@
+namespace Library
+
+module LispVal = 
+
+    type Env = (string * LispVal ref) list ref
+
+    and FuncRecord = {
+        parameters: string list;
+        varargs: string option;
+        body: LispVal list;
+        closure: Env;
+    }
+
+    and LispVal = 
+        | Atom of string
+        | List of LispVal list
+        | DottedList of LispVal list * LispVal
+        | Number of int
+        | String of string
+        | Bool of bool
+        | PrimitiveFunc of (LispVal list -> LispVal)
+        | Func of FuncRecord
+        | Nil
+
+    let rec ShowVal = function
+        | Atom atom -> atom
+        | String str -> "\"" + str + "\""
+        | Number num -> string num
+        | Bool true -> "#t"
+        | Bool false -> "#f"
+        | Nil -> "Nil"
+        | List contents -> "(" + UnwordsList contents + ")"
+        | DottedList (head, tail) -> "(" + UnwordsList head + "." + ShowVal tail + ")"
+        | PrimitiveFunc (_) -> "<primitive>"
+        | Func (func) -> "(lambda (" +
+                         UnwordsList (func.parameters |> List.map String) +
+                         (match func.varargs with
+                            | None -> ""
+                            | Some(arg) -> "." + arg) +
+                         ") ...)"
+
+    and UnwordsList = List.map ShowVal >> String.concat " "
